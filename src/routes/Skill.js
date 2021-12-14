db260b0f552f63a41d642c380085a4110b7947fc
hyeongwoo94 <87638160+hyeongwoo94@users.noreply.github.com>
2021-12-14 16:34:29 +0900

    스킬 완료

--- src/routes/Skill.js
+++ src/routes/Skill.js
@@ -1,50 +1,84 @@
 import Hearder from "../component/Hearder";
-import react from "../img/react.png"
-import css from "../img/css.png"
-import illust from "../img/illust.png"
-import html from "../img/html.png"
-import js from "../img/js.png"
-import sass from "../img/sass.png"
-import photoshop from "../img/photoshop.png"
-import tailwind from "../img/tailwind.png"
+import react from "../img/react.png";
+import css from "../img/css.png";
+import illust from "../img/illust.png";
+import html from "../img/html.png";
+import js from "../img/js.png";
+import sass from "../img/sass.png";
+import photoshop from "../img/photoshop.png";
+import tailwind from "../img/tailwind.png";
 const Skill = () => {
-  return(
+  return (
     <>
-    <Hearder />
-    <div className="max-w-screen-lg mx-auto">
-        <span>스킬 애니메이션 부트스트랩에서 사용한것처럼 깔끔한 것 가져오기</span>
-    </div>
-    <div>
-      <ul className="grid grid-cols-2">
-        <li>
-          <img src={react} alt= "리액트" />
-        </li>
-        <li>
-          <img src={tailwind} alt= "tailwind" />
-        </li>
-        <li>
-          <img src={html} alt= "html" />
-        </li>
-        <li>
-          <img src={css} alt= "css" />
-        </li>
-        <li>
-          <img src={sass} alt= "sass" />
-        </li>
-        <li>
-          <img src={js} alt= "js" />
-        </li>
-        <li>
-          <img src={illust} alt= "illust" />
-        </li>
-        <li>
-          <img src={photoshop} alt= "photoshop" />
-        </li>
-
-      </ul>
-    </div>
+      <Hearder />
+      <div className="max-w-screen-lg mx-auto animate__animated animate__slideInUp">
+        <div className="my-16">
+        <h2>기초 퍼블리싱</h2>
+          <ul className="grid grid-cols-2 gap-12 mt-4">
+            <li className="flex gap-2">
+              <img className="inline-block w-1/6" src={html} alt="html" />
+              <span className="bar_4 my-auto"></span>
+              <span>90%</span>
+            </li>
+            <li className="flex gap-2">
+              <img className="inline-block w-1/6" src={css} alt="css" />
+              <span className="bar_4 my-auto"></span>
+              <span>90%</span>
+            </li>
+            <li className="flex gap-2">
+              <img className="inline-block w-1/6" src={js} alt="js" />
+              <span className="bar_2 my-auto"></span>
+              <span>70%</span>
+            </li>
+          </ul>
+          
+          <hr className="my-8" />
+          <h2>CSS 라이브러리, 프레임 워크</h2>
+          <ul className="grid grid-cols-2 gap-12 mt-4">
+            <li className="flex gap-2">
+              <img
+                className="inline-block w-1/6"
+                src={tailwind}
+                alt="tailwind"
+              />
+              <span className="bar_4 my-auto"></span>
+              <span>90%</span>
+            </li>
+            <li className="flex gap-2">
+              <img className="inline-block w-1/6" src={sass} alt="sass" />
+              <span className="bar_3 my-auto"></span>
+              <span>80%</span>
+            </li>
+            <li className="flex gap-2">
+              <img className="inline-block w-1/6" src={react} alt="리액트" />
+              <span className="bar my-auto"></span>
+              <span>60%</span>
+            </li>
+          </ul>
+          
+          <hr className="my-8" />
+          <h2>디자인 툴</h2>
+          <ul className="grid grid-cols-2 gap-12 mt-4">
+            <li className="flex gap-2">
+              <img
+                className="inline-block w-1/6"
+                src={photoshop}
+                alt="photoshop"
+              />
+              <span className="bar_4 my-auto"></span>
+              <span>90%</span>
+            </li>
+            <li className="flex gap-2">
+              <img className="inline-block w-1/6" src={illust} alt="illust" />
+              <span className="bar_2 my-auto"></span>
+              <span>70%</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div></div>
     </>
-  )
-}
+  );
+};
 
 export default Skill;
