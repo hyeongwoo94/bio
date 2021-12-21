dd6d85624315ac98bef131b7c1583a71d3568771
hyeongwoo94 <87638160+hyeongwoo94@users.noreply.github.com>
2021-12-21 14:36:06 +0900

    ...

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer id="footer" className="bg-gray-800">
       <div className="max-w-screen-lg flex justify-between my-2 mx-auto text-white px-2 md:px-0">
         <p className="text-xl md:block hidden">gudn4338@naver.com</p>
-        <p className="hover:font-extrabold"><a className="hover:text-red-400" href="https://github.com/hyeongwoo94/bio" onClick={"_blank"}> 포트폴리오 코드 보러 가기(제작:React)</a></p>
+        <p className="hover:font-extrabold"><a className="hover:text-red-400" href="https://github.com/hyeongwoo94/bio" onClick={"_blank"}> 코드 보러가기(제작:React)</a></p>
         <p className="hover:font-extrabold">
           <a className="hover:text-red-400" href={file} download="박형우_이력서.hwp" alt="이력서 다운로드">
             <i className="xi-file-o mr-1"></i>
