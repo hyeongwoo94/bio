eb43ce6eb33eb7be9a275c41177a72e0a6ebf7ba
hyeongwoo94 <87638160+hyeongwoo94@users.noreply.github.com>
2021-12-21 09:34:45 +0900

    이력서고치기

--- src/component/SimpleSlider.js
+++ src/component/SimpleSlider.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import me from '../img/me.jpg';
+import my from "../img/profile1.jpeg"
+import my2 from "../img/profile2.jpeg"
+import my3 from "../img/profile3.png"
+import my4 from "../img/profile4.png"
 
 export default class SimpleSlider extends Component {
   render() {
@@ -20,22 +23,16 @@ export default class SimpleSlider extends Component {
       <div>
         <Slider {...settings}>
             <div className="px-4">
-              <img className=" w-auto" src={me} alt="내사진" />
+              <img className="" src={my} alt="내사진" />
             </div>
             <div className="px-4">
-              <img className=" w-auto" src={me} alt="내사진" />
+              <img className="" src={my2} alt="내사진" />
             </div>
             <div className="px-4">
-              <img className=" w-auto" src={me} alt="내사진" />
+              <img className="bg-gray-100 " src={my3} alt="내사진" />
             </div>
             <div className="px-4">
-              <img className=" w-auto" src={me} alt="내사진" />
-            </div>
-            <div className="px-4">
-              <img className=" w-auto" src={me} alt="내사진" />
-            </div>
-            <div className="px-4">
-              <img className=" w-auto" src={me} alt="내사진" />
+              <img className="bg-gray-100 " src={my4} alt="내사진" />
             </div>
         </Slider>
       </div>
